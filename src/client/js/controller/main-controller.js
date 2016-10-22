1f3faa351fae4e84ebff74ddb0a02805762f29c6
Fernando <fernando.godinho@gamesys.co.uk>
2016-10-22 11:37:50 +0100

    CLIENT - Changed the main-controller to forward diconnection and reconnection events from the connectionService to the broadcasterService

--- src/client/js/controller/main-controller.js
+++ src/client/js/controller/main-controller.js
@@ -1,18 +1,37 @@
 var MainView = require('./../view/main-view');
 var ConnectionService = require('./../service/connection-service');
 var BroadcasterService = require('./../service/broadcaster-service');
+var ConnectionEvent = require('./../event/connection-event');
+var NotificationEvent = require('./../event/notification-event');
 
 function Constructor () {
 	this._broadcasterService = new BroadcasterService();
 	this._connectionService = new ConnectionService();
 	this._view = new MainView();
 
+	this._handleConnectionDisconnected = this._handleConnectionDisconnected.bind(this);
+	this._handleConnectionReconnected = this._handleConnectionReconnected.bind(this);
 	this._handleDocumentStateChange = this._handleDocumentStateChange.bind(this);
 }
 
-Constructor.DEFAULT_ADDRESS = 'localhost';
-Constructor.DEFAULT_PORT = 3001;
-Constructor.DEFAULT_PATH = '/server';
+Constructor.prototype._handleConnectionDisconnected = function () {
+	this._broadcasterService.emit(NotificationEvent.DISCONNECTED);
+};
+
+Constructor.prototype._handleConnectionReconnected = function () {
+	this._broadcasterService.emit(NotificationEvent.RECONNECTED);
+};
+
+Constructor.prototype._handleDocumentStateChange = function () {
+	this._checkDocumentReady();
+};
+
+Constructor.prototype._checkDocumentReady = function () {
+	if (document.readyState === 'complete') {
+		document.removeEventListener('readystatechange', this._handleDocumentStateChange);
+		this._view.render(document);
+	}
+};
 
 Constructor.prototype.setup = function (address, port, path) {
 	this._setupConnectionService(address, port, path);
@@ -28,17 +47,13 @@ Constructor.prototype._setupConnectionService = function (address, port, path) {
 	port = port || Constructor.DEFAULT_PORT;
 	path = path || Constructor.DEFAULT_PATH;
 	this._connectionService.setup('ws://' + address + ':' + port + path);
+
+	this._connectionService.on(ConnectionEvent.DISCONNECTED, this._handleConnectionDisconnected);
+	this._connectionService.on(ConnectionEvent.RECONNECTED, this._handleConnectionReconnected);
 };
 
-Constructor.prototype._handleDocumentStateChange = function () {
-	this._checkDocumentReady();
-};
-
-Constructor.prototype._checkDocumentReady = function () {
-	if (document.readyState === 'complete') {
-		document.removeEventListener('readystatechange', this._handleDocumentStateChange);
-		this._view.render(document);
-	}
-};
+Constructor.DEFAULT_ADDRESS = 'localhost';
+Constructor.DEFAULT_PORT = 3001;
+Constructor.DEFAULT_PATH = '/server';
 
 module.exports = Constructor;
--- test/client/suit/controller/main-controller.spec.js
+++ test/client/suit/controller/main-controller.spec.js
@@ -5,6 +5,9 @@ var MainView = require('./../../mockups/view/main-view.mock');
 var ConnectionService = require('./../../mockups/service/connection-service.mock');
 var BroadcasterService = require('./../../mockups/service/broadcaster-service.mock');
 
+var ConnectionEvent = require('./../../../../src/client/js/event/connection-event');
+var NotificationEvent = require('./../../../../src/client/js/event/notification-event');
+
 describe('The Main Controller class', function () {
 	var MainController, sandbox;
 	var address = '127.0.0.1';
@@ -87,7 +90,7 @@ describe('The Main Controller class', function () {
 				instance.setup(address, port, path);
 			});
 
-			it ('should render the view after the dom is loaded with the document as argument', function () {
+			it('should render the view after the dom is loaded with the document as argument', function () {
 				var spy = sandbox.spy(mainView, 'render');
 
 				mockReadystatechangeListener();
@@ -95,13 +98,29 @@ describe('The Main Controller class', function () {
 				spy.should.have.been.calledWith(document);
 			});
 
-			it ('should not render the view if the readyStateChange is not to "complete" state', function () {
+			it('should not render the view if the readyStateChange is not to "complete" state', function () {
 				var spy = sandbox.spy(mainView, 'render');
 
 				document.readyState = 'bogus';
 				mockReadystatechangeListener();
 
 				spy.should.not.have.been.calledWith(document);
+			});
+
+			it('should trigger a NotificationEvent.DISCONNECTED on the broadcasterService when the connectionService fires a ConnectionEvent.DISCONNECTED', function () {
+				var spy = sandbox.spy(broadcasterService, 'emit');
+				
+				connectionService.emit(ConnectionEvent.DISCONNECTED);
+				
+				spy.should.have.been.calledWith(NotificationEvent.DISCONNECTED);
+			});
+
+			it('should trigger a NotificationEvent.RECONNECTED on the broadcasterService when the connectionService fires a ConnectionEvent.RECONNECTED', function () {
+				var spy = sandbox.spy(broadcasterService, 'emit');
+
+				connectionService.emit(ConnectionEvent.RECONNECTED);
+
+				spy.should.have.been.calledWith(NotificationEvent.RECONNECTED);
 			});
 		});
 	});
