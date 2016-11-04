72cfed6d374c46c7f55650065601cff3d586aa6c
Fernando Godinho <cainvampyr@gmail.com>
2016-11-04 08:37:35 +0000

    SERVER - Added the pong route class, mock and spec

---
+++ test/server/suit/route/data/pong-route.spec.js
@@ -0,0 +1,43 @@
+var sinon = require('sinon');
+
+var EventManager = require('./../../../mockups/component/event-manager.mock');
+var Socket = require('./../../../mockups/socket.mock');
+
+var SocketEvent = require('./../../../../../src/server/event/socket-event');
+
+describe('The Pong Route class', function() {
+	var PongRoute, sandbox;
+
+	beforeEach(function () {
+		sandbox = sinon.sandbox.create();
+		PongRoute = require('./../../../../../src/server/route/data/pong-route');
+	});
+
+	it('should be a function', function () {
+		PongRoute.should.be.a('function');
+		sandbox.restore();
+	});
+
+	describe('as an instance', function () {
+		var instance, eventManager, socket;
+
+		beforeEach(function () {
+			socket = new Socket();
+			eventManager = new EventManager();
+			instance = new PongRoute(eventManager);
+		});
+
+		it('should be an instance of PongRoute', function () {
+			instance.should.be.an.instanceOf(PongRoute);
+		});
+
+		it('should invoke the send method of the data socket', function () {
+			var spy = sandbox.spy(eventManager, 'emit');
+			var data = { _socket: socket };
+
+			return instance.execute(data).then(function () {
+				spy.should.have.been.calledWith(SocketEvent.PONG, socket).calledTOnce;
+			});
+		});
+	});
+});
